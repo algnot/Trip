5b9287d5f932e9f469179f46cee0ff14319fa31f
algnot <thanawat.tongla@lmwn.com>
2024-05-19 00:45:55 +0700

    add logout

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,11 +3,13 @@ import Button from "@/components/Button";
 import Navbar from "@/components/Navbar";
 import { logout } from "@/utils/client/apiClient";
 import { useRouter } from "next/navigation";
+import { auth } from "@/utils/client/firebaseClient";
 
 export default function Home() {
   const router = useRouter()
 
   const onLogout = async () => {
+    await auth.signOut()
     await logout()
     router.push("/login")
   }
