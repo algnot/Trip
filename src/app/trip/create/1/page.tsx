0813bb6f217ca2f260f9b3ceab176270bcf04c53
algnot <thanawat.tongla@lmwn.com>
2024-05-19 19:44:45 +0700

    add input

--- src/app/trip/create/1/page.tsx
+++ src/app/trip/create/1/page.tsx
@@ -5,7 +5,7 @@ import ProgressBar from "@/components/ProgressBar";
 import Topbar from "@/components/Topbar";
 import { useRouter } from "next/navigation";
 import React from "react";
-import { IoIosDocument } from "react-icons/io";
+import { IoIosCalendar, IoIosDocument, IoIosText } from "react-icons/io";
 
 export default function CreateTrip() {
   const router = useRouter();
@@ -18,7 +18,7 @@ export default function CreateTrip() {
           router.push("/");
         }}
       />
-      <div className="px-10 pt-8">
+      <form className="px-10 pt-8">
         <ProgressBar steps={[1, 2, 3]} progresses={[50, 0, 0]} />
         <div className="mt-8">
           <Input
@@ -28,8 +28,21 @@ export default function CreateTrip() {
             name="name"
             icon={IoIosDocument}
           />
+          <Input
+            label="วันที่"
+            type="date"
+            name="date"
+            icon={IoIosCalendar}
+          />
+          <Input
+            label="รายละเอียดการหาร"
+            type="textarea"
+            name="desceiption"
+            placeholder="หารค่ากับข้าวที่ไปกินกันที่สยาม"
+            icon={IoIosText}
+          />
         </div>
-      </div>
+      </form>
       <Navbar active="/" />
     </div>
   );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,29 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { IconType } from "react-icons";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { formatDateToString } from "@/utils/client/date";
 
 export default function Input(props: {
   name: string;
-  type: "text" | "number" | "date";
+  type: "text" | "number" | "date" | "textarea";
   label?: string;
   placeholder?: string;
   icon?: IconType;
 }) {
+  const [date, setDate] = useState(new Date());
+
+  if (props.type !== "date") {
+    return (
+      <div>
+        {props.label && (
+          <label className="block mb-3 text-black">{props.label}</label>
+        )}
+
+        <div className="relative mb-6">
+          {props.icon && (
+            <div className={`absolute flex items-center pl-2 pointer-events-none ${props.type === "textarea" ? "pt-2.5" : "inset-y-0 start-0"}`}>
+              {<props.icon className="fill-gray text-gray w-[24px] h-[24px]" />}
+            </div>
+          )}
+          {props.type === "textarea" ? (
+            <textarea 
+            placeholder={props.placeholder}
+            className={`border-2 border-gray text-black text-md rounded-lg block w-full ${
+                props.icon ? "pl-10" : "pl-2"
+              } p-2 focus:border-black disabled:bg-blackground invalid:border-error invalid:text-error focus:invalid:border-error focus:invalid:ring-error`}></textarea>
+          ) : (
+            <input
+              type={props.type}
+              name={props.name}
+              className={`border-2 border-gray text-black text-md rounded-lg block w-full ${
+                props.icon ? "pl-10" : "pl-2"
+              } p-2 focus:border-black disabled:bg-blackground invalid:border-error invalid:text-error focus:invalid:border-error focus:invalid:ring-error`}
+              placeholder={props.placeholder}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       {props.label && (
         <label className="block mb-3 text-black">{props.label}</label>
       )}
-
-      <div className="relative mb-6">
+      <div className="flex mb-6 border-2 border-gray bg-white rounded-lg overflow-hidden">
         {props.icon && (
-          <div className="absolute inset-y-0 start-0 flex items-center pl-2 pointer-events-none">
+          <div className="flex items-center pl-2 pointer-events-none ">
             {<props.icon className="fill-gray w-[24px] h-[24px]" />}
           </div>
         )}
+        <DatePicker
+          selected={date}
+          onChange={(date) => setDate(date ?? new Date())}
+          dateFormat="dd/MM/yyyy"
+          className={` text-black text-md w-96 block p-2 focus:border-transparent outline-none`}
+        />
         <input
           type={props.type}
           name={props.name}
-          className={`border-2 border-gray text-black text-md rounded-lg block w-full ${props.icon ? "pl-10" : "pl-2"} p-2 focus:border-black disabled:bg-blackground invalid:border-error invalid:text-error focus:invalid:border-error focus:invalid:ring-error`}
+          value={formatDateToString(date)}
+          className="invisible"
           placeholder={props.placeholder}
         />
       </div>
