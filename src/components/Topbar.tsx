fa1807205ef3d426ce35a20d8e575db2e7e1141e
algnot <thanawat.tongla@lmwn.com>
2024-05-19 19:50:36 +0700

    fix z-index

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -3,7 +3,7 @@ import { SlArrowLeft } from "react-icons/sl";
 
 export default function Topbar(props: { topic: string; onBack?: () => void }) {
   return (
-    <div className="bg-white shadow-sm p-5 flex items-center gap-4 sticky top-0">
+    <div className="bg-white shadow-sm p-5 flex items-center gap-4 sticky top-0 z-50">
       {props.onBack && (
         <div onClick={props.onBack} className="cursor-pointer">
           <SlArrowLeft className="text-xl" />
